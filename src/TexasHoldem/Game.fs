0c393e8f8c69a629c6fa75c9cf3ba8f91d232528
Flávio Silva <flavio1110@gmail.com>
2017-08-21 22:26:03 +0200

    some refactoring. The code still very ugly. I need to work more on it

--- src/TexasHoldem/Game.fs
+++ src/TexasHoldem/Game.fs
@@ -59,18 +59,16 @@ module TexasHoldemPoker =
     let isSequence (cards : Card[]) =
         let ordernedCards = Array.sortBy(fun (suit, rank) -> rank) cards
         let _, rankFst = ordernedCards.[0]
-        let _, rankLst = ordernedCards.[4]
+        let _, rankLst = ordernedCards |> Array.last
 
         let cardIsSequenceFromLastOne i =
-            let isSeq = 
-                if i = 0 then
-                    true
-                else
-                    let _, prevRank = ordernedCards.[i - 1]
-                    let _, currRank = ordernedCards.[i]
-                    let expectedRank = enum<Rank>((int prevRank) + 1)
-                    expectedRank = currRank
-            isSeq
+            match i with
+            | 0 -> true
+            | _ ->
+                let _, prevRank = ordernedCards.[i - 1]
+                let _, currRank = ordernedCards.[i]
+                let expectedRank = enum<Rank>((int prevRank) + 1)
+                expectedRank = currRank
         
         let isInSequence cards =
             cards 
@@ -78,27 +76,22 @@ module TexasHoldemPoker =
             |> Array.reduce (fun acc isSeq -> isSeq)
 
         match rankFst with
-        | Rank.Ace -> false
-        | Rank.King -> false
-        | Rank.Queen -> false
-        | Rank.Jack -> false
+        | Rank.Ace | Rank.King | Rank.Queen | Rank.Jack -> false
         | Rank.Two ->
-            if rankLst = Rank.Ace then
-                isInSequence (ordernedCards |> Array.take 4)
-            else
-                isInSequence ordernedCards
-        | _ -> 
-            isInSequence ordernedCards
+            match rankLst with
+            | Rank.Ace -> isInSequence (ordernedCards |> Array.take 4)
+            | _ -> isInSequence ordernedCards
+        | _ -> isInSequence ordernedCards
 
     let getHandValue hand = 
         let ordernedCards = 
             hand.Cards
             |> Array.sortBy(fun (suit, rank) -> rank) 
         
-        let suitsGroups = 
-            ordernedCards 
-            |> Array.groupBy(fun (suit, rank) -> suit)
-            |> Array.sortByDescending(fun (key, values) -> values.Length)
+        let hasOnlyOneSuit cards = 
+            cards 
+            |> Array.distinctBy(fun (suit, rank) -> suit)
+            |> Array.length = 1
 
         let ranksGroup = 
             ordernedCards
@@ -107,22 +100,25 @@ module TexasHoldemPoker =
 
         let _, rankFst = ordernedCards.[0]
 
-        let (key, sameRank) = ranksGroup.[0]
-        let haveSuits number = suitsGroups.Length = number
+        let key, sameRank = ranksGroup.[0]
         let haveRanks number = ranksGroup.Length = number        
-        let sequenceStartsWithTen = rankFst = Rank.Ten
-        let isStraight = isSequence ordernedCards
-         
+        let startsWithTen = rankFst = Rank.Ten                
         let topCardWithSameRank = sameRank.Length
-        let isFlush = haveSuits 1
-        let isFourOfAKind = haveRanks 2 && topCardWithSameRank = 4
-        let isRoyalFlush = isFlush && isStraight && sequenceStartsWithTen
-        let isStraightFlush = isFlush && isStraight
-        let isFullHouse = haveRanks 2 && topCardWithSameRank = 3
-        let isThreeOfAKind = haveRanks 3 && topCardWithSameRank = 3
-        let isTwoPairs = haveRanks 3 && topCardWithSameRank = 2
-        let isPair = haveRanks 4 && topCardWithSameRank = 2
+        let groupedAs (differentRanks, occurrences) = 
+            haveRanks differentRanks && topCardWithSameRank = occurrences
 
+        //TODO: The way that I'm creating these expressions is messing up the ifs bellow. 
+        let isStraight = isSequence ordernedCards 
+        let isFlush = hasOnlyOneSuit ordernedCards        
+        let isRoyalFlush = isFlush && isStraight && startsWithTen
+        let isStraightFlush = isFlush && isStraight
+        let isFourOfAKind = groupedAs (2, 4)
+        let isFullHouse = groupedAs (2, 3)
+        let isThreeOfAKind = groupedAs (3, 3)
+        let isTwoPairs = groupedAs (3, 2)
+        let isPair = groupedAs (4, 2)
+
+        //TODO: Figure out a way to improve it. How transorm it in match with?
         let handValue =
             if isRoyalFlush then
                 HandValue.RoyalFlush
@@ -146,4 +142,3 @@ module TexasHoldemPoker =
                 HandValue.HighCard
 
         handValue
-    
